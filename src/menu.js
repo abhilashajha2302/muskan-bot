a7ab08a6451eaec6ad45b1d383e7eb5545972192
abhilashajha2302 <uietece2024abhilashajha@kuk.ac.in>
2022-08-26 03:25:02 +0530

    Muskan Whattsap bot

---
+++ src/menu.js
@@ -0,0 +1,22 @@
+export const menu = {
+  1: {
+    description: 'Grievance about Education',
+    price: 6,
+  },
+  2: {
+    description: 'Grievance about Bank loans',
+    price: 6,
+  },
+  3: {
+    description: 'Grievance about Scholarships',
+    price: 6,
+  },
+  4: {
+    description: 'Grievance about PENCiL Portal',
+    price: 6,
+  },
+  5: {
+    description: 'Other Grievance',
+    price: 6,
+  },
+};
